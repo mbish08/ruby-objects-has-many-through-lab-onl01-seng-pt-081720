033cd27928b731d31fe00a8097f2c74eb4238167
Mikel Bishop <mdrew8@gmail.com>
2020-09-20 23:32:22 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,32 +1,27 @@
-require 'pry'
-
 class Doctor
-  
-  attr_accessor :name 
-  
+
+  attr_accessor :name
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    @@all << self 
-  end 
-  
-  def self.all
-    @@all 
-  end 
-  
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
-  end 
-  
-  def appointments 
+    @@all << self
+  end
+
+  def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
-  end 
-  # binding.pry 
-  
+  end
+
   def patients
     appointments.map {|appointment| appointment.patient}
-  end 
-  
-  #date, patient, doctor
-end 
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+end
